789ffef34ca38fe30abb61bc3535faacb6f3d512
axcionel <52538388+axcionel@users.noreply.github.com>
2025-10-22 13:55:20 -0400

    Update java.js

--- java.js
+++ java.js
@@ -2,6 +2,7 @@ const food = [
   {
     name: "20 Piece McNuggets",
     price: "$4.99",
+    typeof: "Sides",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-20-chicken-mcnuggets:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
     alt: "20 Pieces",
@@ -9,6 +10,7 @@ const food = [
   {
     name: "McChicken",
     price: "$3.89",
+    typeof: "Sandwiches",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202012_0383_CrispyChickenSandwich_PotatoBun_1564x1564-1:nutrition-calculator-tile?resmode=sharp2",
     alt: "fast food",
@@ -16,6 +18,7 @@ const food = [
   {
     name: "Large Fries",
     price: "$3.79",
+    typeof: "Sides",
     Image:
       "https://www.mcdonalds.com.sg/sites/default/files/2025-02/French%20Fries%20-%20L.jpg",
     alt: "fast food",
@@ -23,13 +26,15 @@ const food = [
   {
     name: "McFlurry",
     price: "$4.39",
+    typeof: "Desserts",
     Image:
-      "https://www.pngkit.com/png/full/360-3605214_mcdonalds-oreo-mcflurry-mcdonalds-ice-cream.png",
+      "https://s7d1.scene7.com/is/image/mcdonalds/DC_202002_3832_OREOMcFlurry_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
     alt: " fast food",
   },
   {
     name: "Big Mac",
     price: "$6.72",
+    typeof: "Sandwiches",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202302_0005-999_BigMac_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
     alt: "fast food",
@@ -37,13 +42,15 @@ const food = [
   {
     name: "Snack Wrap",
     price: "$2.99",
+    typeof: "Sides",
     Image:
-      "https://www.tasteofhome.com/wp-content/uploads/2025/06/McDonalds-Snack-Wrap-Returns_Snack_Wrap-Hero_Courtesy-Mcdonalds_DKedit_FT.jpg",
+      "https://s7d1.scene7.com/is/image/mcdonalds/DC_202504_25254_RanchSnackWrap_1564x1564:product-header-mobile?wid=1313&hei=1313&dpr=off",
     alt: "fast food",
   },
   {
     name: "Spicy McCrispy",
     price: "$3.89",
+    typeof: "Sandwiches",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202012_0116_SpicyCrispyChicken_PotatoBun_1564x1564-1?product-header-desktop&wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -51,6 +58,7 @@ const food = [
   {
     name: "Egg McMuffin",
     price: "$3.99",
+    typeof: "Sandwiches",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202004_0046_EggMcMuffin_1564x1564?wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -58,13 +66,15 @@ const food = [
   {
     name: "Sausage Burrito",
     price: "$2.89",
+    typeof: "Sides",
     Image:
-      "https://s7d1.scene7.com/is/image/mcdonalds/DC_202411_0334_SausageBurrito_Alt_McValue_1564x1564?wid=1000&hei=1000&dpr=off",
+      "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-breakfast-burrito-white-tortilla:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
     alt: "fast food",
   },
   {
     name: "Filet-O-Fish",
     price: "$3.99",
+    typeof: "Sandwiches",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202302_5926-999_Filet-O-Fish_HalfSlice_Alt_1564x1564-1?wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -72,6 +82,7 @@ const food = [
   {
     name: "Vanilla Ice Cream",
     price: "$2.99",
+    typeof: "Desserts",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202106_0336_LargeVanillaCone_1564x1564-1:nutrition-calculator-tile?resmode=sharp2",
     alt: "fast food",
@@ -79,6 +90,7 @@ const food = [
   {
     name: "Apple Pie",
     price: "$2.99",
+    typeof: "Desserts",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202004_0706_BakedApplePie_Broken_1564x1564-1?wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -86,6 +98,7 @@ const food = [
   {
     name: "Happy Meal",
     price: "$6.77",
+    typeof: "Combos",
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/TinyTAN_Hamburger_1564x1564?wid=1000&hei=1006&dpr=off",
     alt: "fast food",
@@ -123,4 +136,19 @@ function getCards() {
     });
   });
 }
+
 getCards();
+
+function filterbyType(typeof) {
+  const filteredFood = document.querySelector(".container");
+  filteredFood.forEach((food) => {
+    const FoodType = food.getAttribute("typeof");
+    if (FoodType === typeof) {
+      food.style.display = "";
+    } else {
+      food.style.display = "none";
+    }
+  });
+}
+
+filterbyType("Sandwiches");
