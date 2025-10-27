465602c090229b2a69aa299dea11d0a7c04bcc88
axcionel <52538388+axcionel@users.noreply.github.com>
2025-10-27 14:02:08 -0400

    1

--- java.js
+++ java.js
@@ -3,6 +3,7 @@ const food = [
     name: "20 Piece McNuggets",
     price: "$4.99",
     typeof: "Sides",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-20-chicken-mcnuggets:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
     alt: "20 Pieces",
@@ -11,6 +12,7 @@ const food = [
     name: "McChicken",
     price: "$3.89",
     typeof: "Sandwiches",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202012_0383_CrispyChickenSandwich_PotatoBun_1564x1564-1:nutrition-calculator-tile?resmode=sharp2",
     alt: "fast food",
@@ -19,6 +21,7 @@ const food = [
     name: "Large Fries",
     price: "$3.79",
     typeof: "Sides",
+
     Image:
       "https://www.mcdonalds.com.sg/sites/default/files/2025-02/French%20Fries%20-%20L.jpg",
     alt: "fast food",
@@ -27,6 +30,7 @@ const food = [
     name: "McFlurry",
     price: "$4.39",
     typeof: "Desserts",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202002_3832_OREOMcFlurry_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
     alt: " fast food",
@@ -35,6 +39,7 @@ const food = [
     name: "Big Mac",
     price: "$6.72",
     typeof: "Sandwiches",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202302_0005-999_BigMac_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
     alt: "fast food",
@@ -43,6 +48,7 @@ const food = [
     name: "Snack Wrap",
     price: "$2.99",
     typeof: "Sides",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202504_25254_RanchSnackWrap_1564x1564:product-header-mobile?wid=1313&hei=1313&dpr=off",
     alt: "fast food",
@@ -51,6 +57,7 @@ const food = [
     name: "Spicy McCrispy",
     price: "$3.89",
     typeof: "Sandwiches",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202012_0116_SpicyCrispyChicken_PotatoBun_1564x1564-1?product-header-desktop&wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -59,6 +66,7 @@ const food = [
     name: "Egg McMuffin",
     price: "$3.99",
     typeof: "Sandwiches",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202004_0046_EggMcMuffin_1564x1564?wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -67,6 +75,7 @@ const food = [
     name: "Sausage Burrito",
     price: "$2.89",
     typeof: "Sides",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-breakfast-burrito-white-tortilla:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
     alt: "fast food",
@@ -75,6 +84,7 @@ const food = [
     name: "Filet-O-Fish",
     price: "$3.99",
     typeof: "Sandwiches",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202302_5926-999_Filet-O-Fish_HalfSlice_Alt_1564x1564-1?wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -83,6 +93,7 @@ const food = [
     name: "Vanilla Ice Cream",
     price: "$2.99",
     typeof: "Desserts",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202106_0336_LargeVanillaCone_1564x1564-1:nutrition-calculator-tile?resmode=sharp2",
     alt: "fast food",
@@ -91,6 +102,7 @@ const food = [
     name: "Apple Pie",
     price: "$2.99",
     typeof: "Desserts",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/DC_202004_0706_BakedApplePie_Broken_1564x1564-1?wid=1000&hei=1000&dpr=off",
     alt: "fast food",
@@ -99,6 +111,7 @@ const food = [
     name: "Happy Meal",
     price: "$6.77",
     typeof: "Combos",
+
     Image:
       "https://s7d1.scene7.com/is/image/mcdonalds/TinyTAN_Hamburger_1564x1564?wid=1000&hei=1006&dpr=off",
     alt: "fast food",
@@ -120,9 +133,9 @@ function inject(food) {
       "afterbegin",
       `
       <div class="card" data-type="${food.typeof}">
-        <h2 class="card-header">${food.name}</h2>
+        <h2 class="card-header">card-name="${food.name}"</h2>
         <img class="card-img" src="${food.Image}" alt="${food.alt}" />
-        <h3 class="card-price">${food.price}</h3>
+        <h3 class="card-price">card-price="${food.price}"</h3>
         <button class="button">Add to Cart</button>
       </div>
     `
@@ -159,3 +172,31 @@ function filterbyType(FilteredFoodType) {
   });
 }
 
+function showAll() {
+  const foods = document.querySelectorAll(".card");
+  foods.forEach((food) => {
+    food.style.display = "";
+  });
+}
+
+function cart() {
+  const buttons = document.querySelectorAll(".button");
+  const cartItems = getcartItems();
+
+  buttons.forEach(button => {
+    button.addEventListener("click", function (event) {
+      const card = event.target.closest(".card");
+      const name = card.getAttribute("card-name").textContent;
+      const price = card.getAttribute("card-price").textContent;
+      cartItems.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <div class="cart-item">
+          <h4 class="item-name">${name}</h4>
+          <h5 class="item-price">${price}</h5>
+        </div>
+      `
+      );
+    });
+
+    function
